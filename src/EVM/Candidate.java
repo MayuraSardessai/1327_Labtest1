1f449b1ff4695b950e37ef005197cd75b58bef12
mayura <mayuravsardessai@gmail.com>
2015-07-09 16:06:27 -0700

    version 4:00

--- src/EVM/Candidate.java
+++ src/EVM/Candidate.java
@@ -4,9 +4,25 @@ public class Candidate {
 	String name;
 	int count;
 	
-	Candidate()
+	Candidate(String name, int count)
 	{
 		this.name = name;
+		this.count = count;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
 		this.count = count;
 	}
 
--- src/EVM/ControlUnit.java
+++ src/EVM/ControlUnit.java
@@ -2,5 +2,42 @@ package EVM;
 
 public class ControlUnit {
 	Candidate c[] = new Candidate[5];
+	BallotUnit ballot = new BallotUnit();
+	int i;
+	int total=0;
+	
+	ControlUnit()
+	{
+		
+	}
+	
+	int getTotalNumberOfVotes()
+	{
+		for(i=0;i<5;i++)
+		{
+			total = total + c[i].getCount();
+		}
+		return total;
+	}
+	
+	void closePoll()
+	{
+		System.out.println("Poll Closed!!");
+	}
 
+	void displayCandidateVotes()
+	{
+		for(i=0;i<5;i++)
+		{
+			System.out.println("\nCandidate Name: " + c[i].getName() + " --> Votes: " + c[i].getCount());
+		}
+	}
+	
+	void resetCounter()
+	{
+		for(i=0;i<5;i++)
+		{
+			c[i].setCount(0);
+		}
+	}
 }
